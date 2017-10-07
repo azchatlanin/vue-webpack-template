4095d5f9692a36fe6d3320c3a61851f362580458
Andrey Zimin <zimin6077@gmail.com>
2017-10-07 10:54:06 +0300

    Video-3 is complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ProgressBarPlugin = require('progress-bar-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -9,13 +11,27 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     filename: 'js/[name].js'
   },
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Vue-webpack template step by step',
-      filename: 'test.html',
+      filename: 'index.html',
       inject: true,
       hash: true,
-      template: 'index.html'
-    })
+      template: 'index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+      }
+    }),
+    new webpack.WatchIgnorePlugin([
+      path.join(__dirname, 'node_modules')
+    ]),
+    new ProgressBarPlugin()
   ]
 }
